039feffc0de82f3882c551c8bb6f675faa6fe746
Mark Smith <tntc.tig@gmail.com>
2017-04-01 19:51:24 -0400

    Detect display distro by name first, then generic, then fallthrough.

--- screenfetch-dev.sh
+++ screenfetch-dev.sh
@@ -1545,17 +1545,7 @@ detectshell () {
 
 # Resolution Detection - Begin
 detectres () {
-	if [[ ${distro} != "Mac OS X" && ${distro} != "Cygwin" && "${distro}" != "Msys" && ${distro} != "Haiku" ]]; then
-		if [[ -n ${DISPLAY} ]]; then
-			if type -p xdpyinfo >/dev/null 2>&1; then
-				if [[ "$distro" =~ "BSD" ]]; then
-					xResolution=$(xdpyinfo | sed -n 's/.*dim.* \([0-9]*x[0-9]*\) .*/\1/pg' | tr '\n' ' ')
-				else
-					xResolution=$(xdpyinfo | sed -n 's/.*dim.* \([0-9]*x[0-9]*\) .*/\1/pg' | sed ':a;N;$!ba;s/\n/ /g')
-				fi
-			fi
-		fi
-	elif [[ ${distro} == "Mac OS X" ]]; then
+	if [[ ${distro} == "Mac OS X" ]]; then
 		xResolution=$(system_profiler SPDisplaysDataType | awk '/Resolution:/ {print $2"x"$4" "}')
 		if [[ "$(echo $xResolution | wc -l)" -ge 1 ]]; then
 			xResolution=$(echo $xResolution | tr "\\n" "," | sed 's/\(.*\),/\1/')
@@ -1566,6 +1556,14 @@ detectres () {
 		width=$(screenmode | cut -d ' ' -f2)
 		height=$(screenmode | cut -d ' ' -f3 | tr -d ',')
 		xResolution="$(echo ${width}x${height})"
+	elif [[ -n ${DISPLAY} ]]; then
+	    if type -p xdpyinfo >/dev/null 2>&1; then
+		    if [[ "$distro" =~ "BSD" ]]; then
+			    xResolution=$(xdpyinfo | sed -n 's/.*dim.* \([0-9]*x[0-9]*\) .*/\1/pg' | tr '\n' ' ')
+			else
+				xResolution=$(xdpyinfo | sed -n 's/.*dim.* \([0-9]*x[0-9]*\) .*/\1/pg' | sed ':a;N;$!ba;s/\n/ /g')
+			fi
+		fi
 	else
 		xResolution="No X Server"
 	fi
